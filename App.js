5da9d524f3f6a2beecf751434216a5e9bf6043f8
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2023-12-08 14:08:02 +0700

    feat: initiate home page and navigation

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <StatusBar/>
+      <Stack.Navigator>
+        <Stack.Screen
+            name={"Home"}
+            component={Home}
+            options={{
+              title: "Temuin",
+              headerShown: false
+            }}
+        />
+      </Stack.Navigator>
+      
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
---
+++ screens/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View} from "react-native";
+
+const Home =({navigation}) => {
+    return(
+        <View>
+
+        </View>
+    )
+};
+
+export default Home;
