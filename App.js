87daca596cdd14448c28a3e433a0467fe3aadd11
andiprtm <moch.andi.21@student.if.ittelkom-sby.ac.id>
2023-12-08 14:10:44 +0700

    fix: delete style on app.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Home from "./screens/home"
 
 const Stack = createNativeStackNavigator();
 
@@ -23,12 +24,3 @@ export default function App() {
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
